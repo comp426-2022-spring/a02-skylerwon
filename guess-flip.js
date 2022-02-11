9736667c7cf9800db72948839d458c3ec993d0e6
yuumisa <skywon@live.unc.edu>
2022-02-10 22:59:23 -0500

    fixed period

--- guess-flip.js
+++ guess-flip.js
@@ -4,15 +4,16 @@ import {coinFlip, coinFlips, countFlips, flipACoin} from './modules/coin.mjs';
 const args = minimist(process.argv.slice(2));
 args['call']
 const call = args.call
-if (call == undefined) {
-    console.error("Error: no input");
+/*if (call == undefined) {
+    console.error("Error: no input.");
     console.error("Usage: node guess-flip --call=[heads|tails]");
     process.exit(1)
-}
+}*/
 
 if(call != 'tails' && call != 'heads') {
-    console.error("Error: no input");
+    console.error("Error: no input.");
     console.error("Usage: node guess-flip --call=[heads|tails]");
+    process.exit(1);
 }
 
 var output4 = flipACoin(call);
